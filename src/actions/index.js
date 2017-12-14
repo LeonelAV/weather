5792f6d7c0375dc97d1400955b9cc75e55a467e7
LeonelAV <meirinho_leonel@hotmail.com>
2017-12-14 17:39:24 +0100

    Redux-promisein Practice

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY =  '64163f22e510550b96a3a600a38a1e0f';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
